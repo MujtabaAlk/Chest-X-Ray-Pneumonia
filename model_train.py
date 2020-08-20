46cb5bda91a1058364e20da9d09f019e087ca703
Scourvinate <mojtaba989@gmail.com>
2020-08-20 23:47:09 +0300

    Network changes: - added 4 Convolutional layers (with max pooling layers) - change convolutional kernel to 5*5 form 3*3 - add 5 more Densely connected layers with sizes 128, 128, 64, 64, 32, 32 nodes respectively

--- model_train.py
+++ model_train.py
@@ -37,17 +37,55 @@ if __name__ == '__main__':
     # create neural network (model)
     model = Sequential()
     # add first Conv layer
-    model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+    model.add(Conv2D(32, (5, 5), input_shape=X.shape[1:]))
     model.add(Activation("relu"))
     model.add(MaxPool2D(pool_size=(2, 2)))
     # add second Conv layer
-    model.add(Conv2D(64, (3, 3)))
+    model.add(Conv2D(32, (5, 5)))
     model.add(Activation("relu"))
     model.add(MaxPool2D(pool_size=(2, 2)))
-    # add Dense layer
+    # add third Conv layer
+    model.add(Conv2D(64, (5, 5)))
+    model.add(Activation("relu"))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    # add forth Conv layer
+    model.add(Conv2D(64, (5, 5)))
+    model.add(Activation("relu"))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    # add fifth Conv layer
+    model.add(Conv2D(128, (5, 5)))
+    model.add(Activation("relu"))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    # add sixth Conv layer
+    model.add(Conv2D(128, (5, 5)))
+    model.add(Activation("relu"))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    # add Flatten layer to flatten data for Dense layers
     model.add(Flatten())
+    # add first Dense layer
+    model.add(Dense(128))
+    model.add(Activation("relu"))
+    # model.add(Dropout(0.5))
+    # add second Dense layer
+    model.add(Dense(128))
+    model.add(Activation("relu"))
+    # model.add(Dropout(0.5))
+    # add third Dense layer
     model.add(Dense(64))
     model.add(Activation("relu"))
+    # model.add(Dropout(0.5))
+    # add forth Dense layer
+    model.add(Dense(64))
+    model.add(Activation("relu"))
+    # model.add(Dropout(0.5))
+    # add fifth Dense layer
+    model.add(Dense(32))
+    model.add(Activation("relu"))
+    # model.add(Dropout(0.5))
+    # add sixth Dense layer
+    model.add(Dense(32))
+    model.add(Activation("relu"))
+    # model.add(Dropout(0.5))
     # add output layer
     model.add(Dense(1))
     model.add(Activation("sigmoid"))
