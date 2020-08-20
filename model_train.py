8e57a486688f72ee1b2fd31cd5a4950dd1917291
Scourvinate <mojtaba989@gmail.com>
2020-08-20 23:57:43 +0300

    Switched activation function from independent object to being added as a variable in layer definition

--- model_train.py
+++ model_train.py
@@ -2,7 +2,7 @@ import pickle
 import time
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Activation, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
 from tensorflow.keras.callbacks import TensorBoard
 
 input_dataset_path = "./images/chest_xray/train/data.pickle"
@@ -37,58 +37,45 @@ if __name__ == '__main__':
     # create neural network (model)
     model = Sequential()
     # add first Conv layer
-    model.add(Conv2D(32, (5, 5), input_shape=X.shape[1:]))
-    model.add(Activation("relu"))
+    model.add(Conv2D(32, (5, 5), activation="relu", input_shape=X.shape[1:]))
     model.add(MaxPool2D(pool_size=(2, 2)))
     # add second Conv layer
-    model.add(Conv2D(32, (5, 5)))
-    model.add(Activation("relu"))
+    model.add(Conv2D(32, (5, 5), activation="relu"))
     model.add(MaxPool2D(pool_size=(2, 2)))
     # add third Conv layer
-    model.add(Conv2D(64, (5, 5)))
-    model.add(Activation("relu"))
+    model.add(Conv2D(64, (5, 5), activation="relu"))
     model.add(MaxPool2D(pool_size=(2, 2)))
     # add forth Conv layer
-    model.add(Conv2D(64, (5, 5)))
-    model.add(Activation("relu"))
+    model.add(Conv2D(64, (5, 5), activation="relu"))
     model.add(MaxPool2D(pool_size=(2, 2)))
     # add fifth Conv layer
-    model.add(Conv2D(128, (5, 5)))
-    model.add(Activation("relu"))
+    model.add(Conv2D(128, (5, 5), activation="relu"))
     model.add(MaxPool2D(pool_size=(2, 2)))
     # add sixth Conv layer
-    model.add(Conv2D(128, (5, 5)))
-    model.add(Activation("relu"))
+    model.add(Conv2D(128, (5, 5), activation="relu"))
     model.add(MaxPool2D(pool_size=(2, 2)))
     # add Flatten layer to flatten data for Dense layers
     model.add(Flatten())
     # add first Dense layer
-    model.add(Dense(128))
-    model.add(Activation("relu"))
+    model.add(Dense(128, activation="relu"))
     # model.add(Dropout(0.5))
     # add second Dense layer
-    model.add(Dense(128))
-    model.add(Activation("relu"))
+    model.add(Dense(128, activation="relu"))
     # model.add(Dropout(0.5))
     # add third Dense layer
-    model.add(Dense(64))
-    model.add(Activation("relu"))
+    model.add(Dense(64, activation="relu"))
     # model.add(Dropout(0.5))
     # add forth Dense layer
-    model.add(Dense(64))
-    model.add(Activation("relu"))
+    model.add(Dense(64, activation="relu"))
     # model.add(Dropout(0.5))
     # add fifth Dense layer
-    model.add(Dense(32))
-    model.add(Activation("relu"))
+    model.add(Dense(32, activation="relu"))
     # model.add(Dropout(0.5))
     # add sixth Dense layer
-    model.add(Dense(32))
-    model.add(Activation("relu"))
+    model.add(Dense(32, activation="relu"))
     # model.add(Dropout(0.5))
     # add output layer
-    model.add(Dense(1))
-    model.add(Activation("sigmoid"))
+    model.add(Dense(1, activation="sigmoid"))
     # compile model
     model.compile(loss="binary_crossentropy",
                   optimizer="adam",
